1e40225db92052932c8de29920c45949497d08bb
Rahil Faiz Syed <rsyed@sagaxtech.com>
2018-10-22 10:13:51 +0100

    removed cred

--- DB_code/DB.sh
+++ DB_code/DB.sh
@@ -6,17 +6,7 @@ FLYWAY_HOME=/home/centos/flyway-5.1.4
 #Configuration definition
 #ENVIRONMENT    APPLICATION user_owner		usr_owner_pwd 		user_app01 			user_app01_pwd		user_ro 		user_ro_pwd
 cat > dbcreds.$$ <<EOF
-integration      PROVLOC      provloc_int      pr0vL0c_summ1t26      provloc_int_app01           pr0vL0c_h1LLs1d318           provloc_int_ro           Jaguar_1s_fas2
-integration      RECRED       recred_int           Recr3d_ford_Tauru5     recred_int_app01           Recr3d_S3ntra12           recred_int_ro           Recr3d_Ni55an
-integration      CMSRM        cmsrm_int           cm0vL0c_int007           cmsrm_int_app01           cm0vL0c_int8pp422           cmsrm_int_ro           CMS7tah_fast3r
-integration      ASSET        asset_plat_int      SgxL01a55et_plat08      asset_plat_int_app01      Wat3rguni5saf3                asset_plat_int_ro      Dark8night
-integration      VERDIN       verdin_int           v3RDin_plat24           verdin_int_app01           Wat3rguni5saf3_8pp           verdin_int_ro           Dark8night_r00615
-staging          PROVLOC      provloc_stg      pr0vL0c_m33t97      provloc_stg_app01           pr0vL0c_m0unta1n           provloc_stg_ro      d33p_sl0p321
-staging          CMSRM        cmsrm_stg           unset                     cmsrm_stg_app01           cm0vL0c_int8pp422           cmsrm_stg_ro           CMS789_sp33dier
-staging          VERDIN       verdin_stg           v3RDin_plat0615          verdin_stg_app01           Wat3rguni5saf3_8pp0615      verdin_stg_ro           Dark8night_r00
-staging          RECRED       recred_stg           Recr3d_Ch3vy_Talibu     	recred_stg_app01           Recr3d_L3xu5           	recred_stg_ro   		Recr3d_T3sl8
-production       PROVLOC      provloc_prd      ADD_BEFORE_DEPLOYMENT      	provloc_prd_app01           pr0vL0c_cr33k200           provloc_prd_ro   	sL1pp3ry_sL0pe
-production       RECRED       recred_prd       ADD_BEFORE_DEPLOYMENT  		recred_prd_app01           Recr3d_sp8c3craf2           	recred_prd_ro   	Recr3d_ecl1pse
+
 EOF
 
 CONFIG_FOUND=false
